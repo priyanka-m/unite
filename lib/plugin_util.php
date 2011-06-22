22ad96be0717fb95b48d314ffc7ba37db81bb609
Priyanka M <priyanka.menghani@gmail.com>
2011-06-22 22:32:02 +0530

    a class for plugins

--- lib/plugin_util.php
+++ lib/plugin_util.php
@@ -1,27 +1,29 @@
 <?php
+
 class Plugins_Util{
 	private static $dbconnection=false;
 	function plugins_util(){
 	}
 	public static function connect_OC_DB(){
 		$connect=OC_DB::connect();
-		if($connect){
-			$dbconnection=true;
+		/*if($connect){
+			$dbconnetion=true;
 			return true;
 		}
 		else{
 			return false;
-		}
+		}*/
 		
 	}
 	
 	public static function construct_unite_tables(){
-			$query="create table unite1
+			$query="create table unite
 			(oc_username varchar(20),
-			 unite_service_name varchar(20),
-			 unite_service_user varchar(20),
+			 service_name varchar(20),
+			 service_user varchar(20),
+			 resource varchar(50),
 			 sync numeric(2),
-			 fetch_at timestamp,
+			 fetch_at time,
 			 cron_pattern varchar(5)
 		 )";
 		 $prepared=OC_DB::prepare($query);
@@ -40,13 +42,13 @@ class Plugins_Util{
 		return $result;
 	}
 	public static function disconnect_OC_DB(){
-		if($dbconnection){
+		if(Plugins_Util::$dbconnection){
 			$dbconnection=OC_DB::disconnect;
 		}
-		return $dbconnection;
+		
 	}
 	public static function check_unite_tables_setup(){
-		$query=("show tables like 'unite1' ");
+		$query=("show tables like 'unite' ");
 		$exists=OC_DB::query($query);
 		return $exists;
 		/*if($exists){
@@ -55,6 +57,9 @@ class Plugins_Util{
 		else{
 			return false;
 		}*/
+	}
+	public static function OC_DB_insertid(){
+		OC_DB::insertid();
 	}
 }
 ?>
