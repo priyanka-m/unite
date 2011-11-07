b616ab1cd79c1b40e4ea9b680ba0d4ada1c996bf
Priyanka <priyanka.menghani@gmail.com>
2011-11-07 09:49:06 -0500

    Changes in the past one week

--- cron.php
+++ cron.php
@@ -1,23 +1,20 @@
 <?php
-  require_once('../../lib/base.php');
-  /* 
-	cron script for unite
-  */
-  
-  function require_plugin($plugin) {
+require_once('../../lib/base.php');
+ 
+function require_plugin($plugin) {
 	$file = '../unite_'.$plugin.'/cron_download.php';
-	if(!file_exists($file)) return false;
+	if(!file_exists($file))
+		return false;
 	include($file);
 	if(!function_exists($plugin.'_cron_download')) return false;
 	return true;
-  }
+}
   
-  //TODO: Get the plugins array dynamically.
-  $plugins = array(0=>'flickr', 1=>'googledocs');
-  foreach($plugins as $plugin) {
-    if(require_plugin($plugin)) {
-	  $plugin_download_function = $plugin.'_cron_download';
-	  $plugin_download_function();
+$plugins = array(0=>'flickr', 1=>'googledocs');
+foreach($plugins as $plugin){
+	if(require_plugin($plugin)){
+		$plugin_download_function = $plugin.'_cron_download';
+		$plugin_download_function();
 	}
-  }
+}
 ?>
